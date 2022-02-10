1317b60c25fdb922fd0bbb64b1f87be3760853c3
sansy98 <60301258+sansy98@users.noreply.github.com>
2022-02-10 15:32:13 +0100

    Update 05.ps1

--- 05.ps1
+++ 05.ps1
@@ -1,3 +1,5 @@
 #Assigna a la variable $a el Id del procés que té per nom 'bash'
 
-Get-Process -ProcessName bash | Select-Object -ExpandProperty Id
+#Get-Process -ProcessName bash | Select-Object -ExpandProperty Id
+
+Get-Process bash | select -expand id
